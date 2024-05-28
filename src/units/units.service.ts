1908c2fe283ab8f43fbffdcc00bae1d8b837baf2
Hade21 <abdurrohmanmuhammad123@gmail.com>
2024-05-28 19:00:12 +0800

    feat:[units]-setup units module

---
+++ src/units/units.service.ts
@@ -0,0 +1,14 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+import { PrismaService } from '../common/prisma.service';
+import { ValidationService } from '../common/validation.service';
+
+@Injectable()
+export class UnitsService {
+  constructor(
+    private prismaService: PrismaService,
+    private validationService: ValidationService,
+    @Inject(WINSTON_MODULE_PROVIDER) private logger: Logger,
+  ) {}
+}
