958a31263b067296712d902315ec573162a2cfb0
Hade21 <abdurrohmanmuhammad123@gmail.com>
2024-07-22 19:26:42 +0800

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  app.enableCors();
   const options = new DocumentBuilder()
     .setTitle('Geolocation API')
     .setDescription("Geolocation API for mapping unit's location")
