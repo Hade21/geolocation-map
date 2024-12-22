d54c03fb7451c46f9ddb24ede262a98cbbad7390
Hade21 <abdurrohmanmuhammad123@gmail.com>
2024-12-22 22:03:11 +0800

    feat: add mail module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { LocationModule } from './locations/location.module';
+import { MailModule } from './mail/mail.module';
 import { UnitsModule } from './units/units.module';
 import { UsersModule } from './users/users.module';
-import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
