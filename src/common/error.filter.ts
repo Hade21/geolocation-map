2483c38333bf32e28695879f02697c3cf197dda1
hade21 <abdurrohmanmuhammad123@gmail.com>
2025-06-20 06:09:37 +0800

    fix: standarisasi error response

--- src/common/error.filter.ts
+++ src/common/error.filter.ts
@@ -3,6 +3,7 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { ZodError } from 'zod';
 
@@ -11,18 +12,29 @@ export class ErrorFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse();
 
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Internal Server Error';
+
     if (exception instanceof HttpException) {
-      response.status(exception.getStatus()).json({
-        errors: exception.getResponse(),
-      });
+      status = exception.getStatus();
+      const exceptionResponse = exception.getResponse();
+
+      if (typeof exceptionResponse === 'object') {
+        message = exceptionResponse['message'] || exception.message;
+      } else {
+        message = exceptionResponse as string;
+      }
     } else if (exception instanceof ZodError) {
-      response.status(400).json({
-        errors: 'Validation Error',
-      });
-    } else {
-      response.status(500).json({
-        errrors: exception.message,
-      });
+      status = HttpStatus.BAD_REQUEST;
+      message = exception.errors.map((err) => err.message).join(', ');
+    } else if (exception instanceof Error) {
+      message = exception.message;
     }
+
+    response.status(status).json({
+      status: 'error',
+      statusCode: status,
+      message,
+    });
   }
 }
